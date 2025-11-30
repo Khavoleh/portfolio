70b18cff0aa993a6336157f5f922f0d00fd65670
Oleh Khavar <olehkhavar@gmail.com>
2025-11-30 10:29:50 +0200

    add prettier

---
+++ astro.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+import type { AstroUserConfig } from 'astro';
+
+const config: AstroUserConfig = {};
+
+export default defineConfig(config);
---
+++ prettier.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'prettier';
+
+const config: Config = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+
+  plugins: ['prettier-plugin-astro'],
+
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
+
+export default config;
