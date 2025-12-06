7fc4350b9fe90f394747a75602b24d7c7102e659
Oleh Khavar <olehkhavar@gmail.com>
2025-12-06 10:33:43 +0200

    patch: fix breaking page highlighting

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,10 +1,10 @@
 export const PAGE_URLS = {
   HOME: '/',
-  EXPERIENCE: '/experience',
-  PROJECTS: '/projects',
-  CONTACTS: '/contacts',
-  PRIVACY_POLICY: '/privacy-policy',
-  TERMS_AND_CONDITIONS: '/terms-and-conditions',
+  EXPERIENCE: '/experience/',
+  PROJECTS: '/projects/',
+  CONTACTS: '/contacts/',
+  PRIVACY_POLICY: '/privacy-policy/',
+  TERMS_AND_CONDITIONS: '/terms-and-conditions/',
 };
 
 export const SOCIALS = {
