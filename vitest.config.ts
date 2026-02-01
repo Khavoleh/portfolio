30f954ce61e43a2b4d8cdf8c24785b630264653a
Oleh Khavar <olehkhavar@gmail.com>
2026-02-01 15:16:39 +0200

    feat: Add Vitest for testing helpers

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    include: ['src/**/*.test.ts'],
+  },
+  resolve: {
+    alias: {
+      '@shared': resolve(__dirname, './src/shared'),
+      '@layouts': resolve(__dirname, './src/layouts'),
+      '@components': resolve(__dirname, './src/components'),
+      '@assets': resolve(__dirname, './src/assets'),
+    },
+  },
+});
