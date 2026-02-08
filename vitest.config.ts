aa8a72efe2766401f4d5b2d8466fc3e16ce9be30
Oleh Khavar <olehkhavar@gmail.com>
2026-02-08 12:36:54 +0200

    rename layouts folder to layout

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@shared': resolve(__dirname, './src/shared'),
-      '@layouts': resolve(__dirname, './src/layouts'),
+      '@layout': resolve(__dirname, './src/layouts'),
       '@components': resolve(__dirname, './src/components'),
       '@assets': resolve(__dirname, './src/assets'),
     },
