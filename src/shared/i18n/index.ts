d9ed1848ab3cedb1619529401fc107f4f249b60a
Oleh Khavar <olehkhavar@gmail.com>
2026-02-24 16:05:53 +0200

    Add schema org to 404

--- src/shared/i18n/index.ts
+++ src/shared/i18n/index.ts
@@ -1,7 +1,7 @@
 export { CONTACTS_I18N } from './contacts-i18n';
 export { EXPERIENCE_I18N } from './experience-i18n';
 export { HOME_I18N } from './home-i18n';
+export { NOT_FOUND_I18N } from './not-found-i18n';
 export { PRIVACY_POLICY_I18N } from './privacy-policy-i18n';
 export { PROJECTS_I18N } from './projects-i18n';
-export { SEO_I18N } from './seo-i18n';
 export { TERMS_AND_CONDITIONS_I18N } from './terms-and-conditions-i18n';
--- src/shared/i18n/not-found-i18n.ts
+++ src/shared/i18n/not-found-i18n.ts
@@ -1,14 +1,14 @@
 import type { I18N } from '@shared/interfaces';
 
-export const SEO_I18N: I18N = {
+export const NOT_FOUND_I18N: I18N = {
   en: {
-    notfound_title: 'Page Not Found | Oleh Khavar',
-    notfound_description:
+    title: 'Page Not Found | Oleh Khavar',
+    description:
       'The page you are looking for could not be found. Return to the homepage or explore other sections of the portfolio.',
   },
   uk: {
-    notfound_title: 'Сторінку не знайдено | Олег Хавар',
-    notfound_description:
+    title: 'Сторінку не знайдено | Олег Хавар',
+    description:
       'Сторінку, яку ви шукаєте, не знайдено. Поверніться на головну сторінку або дослідіть інші розділи портфоліо.',
   },
 };
