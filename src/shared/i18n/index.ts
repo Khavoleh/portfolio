eca82e1124d5bd4928e4ce86c37eb89aaa5f32ab
Oleh Khavar <olehkhavar@gmail.com>
2026-02-24 15:35:22 +0200

    Add schema org to contacts

--- src/shared/i18n/index.ts
+++ src/shared/i18n/index.ts
@@ -1,3 +1,4 @@
+export { CONTACTS_I18N } from './contacts-i18n';
 export { EXPERIENCE_I18N } from './experience-i18n';
 export { HOME_I18N } from './home-i18n';
 export { PROJECTS_I18N } from './projects-i18n';
--- src/shared/i18n/seo-i18n.ts
+++ src/shared/i18n/seo-i18n.ts
@@ -2,10 +2,6 @@ import type { I18N } from '@shared/interfaces';
 
 export const SEO_I18N: I18N = {
   en: {
-    contacts_title: 'Contacts | Oleh Khavar',
-    contacts_description:
-      'Contact me for collaboration opportunities, project inquiries, or just to connect. Find my email, LinkedIn, GitHub, and other social profiles.',
-
     privacy_title: 'Privacy Policy | Oleh Khavar',
     privacy_description:
       'Learn about how we collect, use, and protect your personal information when you visit this website.',
@@ -19,10 +15,6 @@ export const SEO_I18N: I18N = {
       'The page you are looking for could not be found. Return to the homepage or explore other sections of the portfolio.',
   },
   uk: {
-    contacts_title: "Зв'яжіться зі мною | Олег Хавар",
-    contacts_description:
-      "Зв'яжіться зі мною для співпраці, запитів щодо проектів або просто для спілкування. Знайдіть мою електронну пошту, LinkedIn, GitHub та інші соціальні профілі.",
-
     privacy_title: 'Політика Конфіденційності | Олег Хавар',
     privacy_description:
       'Дізнайтеся про те, як ми збираємо, використовуємо та захищаємо вашу особисту інформацію під час відвідування цього веб-сайту.',
