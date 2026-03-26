3fb7dd9fe319e5b1b320f27271d60ba01de18314
Oleh Khavar <olehkhavar@gmail.com>
2026-03-26 22:56:20 +0200

    Add new command to package.json

--- shared-astro-config.ts
+++ shared-astro-config.ts
@@ -2,6 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import type { AstroUserConfig } from 'astro';
 
 export const SHARED_ASTRO_CONFIG: AstroUserConfig = {
+  markdown: {
+    syntaxHighlight: 'prism',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
