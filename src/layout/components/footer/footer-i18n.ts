9d11114aa5fbebf3319551072b9cb4b192643fd9
Oleh Khavar <olehkhavar@gmail.com>
2026-03-31 22:16:06 +0300

    patch: Add e2e tests for accessibility check

--- src/layout/components/footer/footer-i18n.ts
+++ src/layout/components/footer/footer-i18n.ts
@@ -5,10 +5,16 @@ export const FOOTER_I18N: I18N = {
     copyright: 'Oleh Khavar. All Rights Reserved.',
     privacy_policy: 'Privacy Policy',
     terms_and_conditions: 'Terms & Conditions',
+    legal_links: 'Legal links',
+    social_links: 'Social links',
+    navigate_to: 'Navigate to',
   },
   uk: {
     copyright: 'Олег Хавар. Усі права захищено.',
     privacy_policy: 'Політика конфіденційності',
     terms_and_conditions: 'Умови та положення',
+    legal_links: 'Юридичні посилання',
+    social_links: 'Соціальні посилання',
+    navigate_to: 'Перейти до',
   },
 };
