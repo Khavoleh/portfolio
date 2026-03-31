8c90d09eec733bfe689f7be560a59ce790fb2035
Oleh Khavar <olehkhavar@gmail.com>
2026-03-31 21:21:32 +0300

    fix: Fix strong updated date

--- src/components/privacy-policy/last-updated/last-updated-i18n.ts
+++ src/components/privacy-policy/last-updated/last-updated-i18n.ts
@@ -2,9 +2,9 @@ import type { I18N } from '@shared/interfaces';
 
 export const LAST_UPDATED_I18N: I18N = {
   en: {
-    privacy_updated: '<strong>Last updated: March 28, 2026</strong>',
+    privacy_updated: 'Last updated: March 28, 2026',
   },
   uk: {
-    privacy_updated: '<strong>Останнє оновлення: 28 березня 2026 р.</strong>',
+    privacy_updated: 'Останнє оновлення: 28 березня 2026 р.',
   },
 };
