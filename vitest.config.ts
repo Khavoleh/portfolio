66321816895e549fb9f9130d0372ba0a16d3e6dc
Oleh Khavar <olehkhavar@gmail.com>
2026-04-15 21:14:54 +0300

    Rename unit and e2e tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    include: ['src/**/*.test.ts'],
+    include: ['src/**/*.unit.ts'],
   },
   resolve: {
     alias: {
